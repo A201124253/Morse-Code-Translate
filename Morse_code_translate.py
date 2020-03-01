d06e7a866798d265490d4509227318d5fca19be9
A201124253 <jimmy-lee_93.06@hotmail.com>
2020-03-01 16:21:37 +0100

    class

--- Morse_code_translate.py
+++ Morse_code_translate.py
@@ -10,54 +10,59 @@ with open(json_filename) as f:
 m2l = {v:k for k,v in morse_table.items()}
 # print(m2l)
 
-# 编码函数
-def encode(message):
-	# 循环每个字母找到对应的点
-	message = message.lower()
-	mes = ''
-	for l in message:
-		# 不等于空格的时候进行翻译
-		if l!=' ':
-			mes = mes + l2m[l] + ' '
-		else:
-			mes = mes + l + l
-	return mes
+class Morse_code_translate():
+	def __init__(self, inhalt):
+		self.inhalt = inhalt
+
+	# 编码函数
+	def encode(self):
+		# 循环每个字母找到对应的点
+		message = self.inhalt.lower()
+		mes = ''
+		for l in message:
+			# 不等于空格的时候进行翻译
+			if l!=' ':
+				mes = mes + l2m[l] + ' '
+			else:
+				mes = mes + l + l
+		return mes
 
 
-# 解码函数
-def decode(message):
-	# 循环每条编码得到字母
-	# m = '['+message+']'
-	# print(m)
-	m1 = message.replace('   ','_')
-	# print('m1={}'.format(m1))
-	m2 = m1.replace(' ', "','")
-	# print('m2={}'.format(m2))
-	m3 = m2.replace('_', "'],['")
-	# print('m3={}'.format(m3))
-	m4 = "[['" + m3 +"']]"
-	# print('m4={}'.format(m4))
-	mess = eval(m4)
-	# print(m4)
+	# 解码函数
+	def decode(self):
+		# 循环每条编码得到字母
+		# m = '['+message+']'
+		# print(m)
+		m1 = self.inhalt.replace('   ','_')
+		# print('m1={}'.format(m1))
+		m2 = m1.replace(' ', "','")
+		# print('m2={}'.format(m2))
+		m3 = m2.replace('_', "'],['")
+		# print('m3={}'.format(m3))
+		m4 = "[['" + m3 +"']]"
+		# print('m4={}'.format(m4))
+		mess = eval(m4)
+		# print(m4)
 
-	# words
-	w = ''
-	for word in mess:
-		for l in word:
-			w = w + m2l[l]
+		# words
+		w = ''
+		for word in mess:
+			for l in word:
+				w = w + m2l[l]
 
-		w = w + ' '
-	w = w + '\n'		
-	return w 
+			w = w + ' '
+		w = w + '\n'		
+		return w 
 	
 if __name__ == "__main__":
 # def main():
 	inhalt = input("please write your words:\n")
+	mst = Morse_code_translate(inhalt)
 	if (inhalt[0]=='.' or inhalt[0]=='-'):
 		print('The words is: ')
-		print(decode(inhalt))
+		print(mst.decode())
 	else:
 		print('The Morse code is: ')
-		print(encode(inhalt))
+		print(mst.encode())
 
 
